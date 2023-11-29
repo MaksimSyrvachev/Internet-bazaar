0ad761ef63841ee5cf4706d01651c5c3df97e330
Patrik Čangel <493182@mail.muni.cz>
2023-11-29 17:29:28 +0100

    feat: changing api

---
+++ src/app/api/favorites/route.ts
@@ -0,0 +1,94 @@
+import { z } from 'zod';
+
+import { db } from '@/server/db';
+import {putFavoriteSchema} from "@/validators/favorite";
+
+export const GET = async (request: Request) => {
+	const { searchParams } = new URL(request.url);
+	const userIdSchema = z.string();
+	const result = userIdSchema.safeParse(searchParams.get('userId'));
+	if (!result.success) {
+		throw new Error('User not specified');
+	} else {
+		const user = await db.user.findUnique({
+			where: {
+				id: result.data
+			},
+			include: {
+				favoriteAds: true,
+				favoriteAuctions: {
+					include: {
+						bids: {
+							orderBy: {
+								amount: 'desc'
+							},
+							take: 1
+						}
+					}
+				}
+			}
+		});
+		const ads = user?.favoriteAds ?? [];
+		const auctions= user?.favoriteAuctions ?? [];
+		return Response.json([...ads, ...auctions]);
+	}
+};
+
+
+export const DELETE = async (request: Request) => {
+	const req = await request.json();
+	const result = putFavoriteSchema.safeParse(req);
+	if (!result.success) {
+		throw new Error('Invalid data from front-end');
+	} else {
+		const updatedUser = await db.user.update({
+			where: {
+				id: result.data.userId
+			},
+			data: result.data.adId
+				? {
+					favoriteAds: {
+						disconnect: {
+							id: result.data.adId
+						}
+					}
+				}
+				: {
+					favoriteAuctions: {
+						disconnect: {
+							id: result.data.auctionId
+						}
+					}
+				}
+		});
+		return Response.json(updatedUser);
+	}
+};
+
+export const PUT = async (request: Request) => {
+	const req = await request.json();
+	const result = putFavoriteSchema.safeParse(req);
+	if (!result.success) {
+		throw new Error('Invalid data from front-end');
+	} else {
+		const updatedUser = await db.user.update({
+			where: {
+				id: result.data.userId
+			},
+			data: result.data.adId
+				? {
+					favoriteAds: {
+						connect: [{ id: result.data.adId }]
+					}
+				}
+				: {
+					favoriteAuctions: {
+						connect: [{ id: result.data.auctionId }]
+					}
+				}
+		});
+
+		return Response.json(updatedUser);
+	}
+};
+
