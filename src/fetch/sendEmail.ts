e610ae1ebd10340c226dba3daab242fff401b95c
Patrik Čangel <493182@mail.muni.cz>
2023-12-05 18:04:05 +0100

    fix: fix email path

--- src/fetch/sendEmail.ts
+++ src/fetch/sendEmail.ts
@@ -3,7 +3,7 @@ import { STATUS_CODES } from 'http';
 import { type SendingEmail } from '@/types/sendingEmail';
 
 export const sendEmail = async (email: SendingEmail) => {
-	const response = await fetch(`/api/emaila`, {
+	const response = await fetch(`/api/email`, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(email)
