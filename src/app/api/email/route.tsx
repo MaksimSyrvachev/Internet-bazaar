069746c4446316d8cd174796465c8b7b70c5f90a
Patrik Čangel <493182@mail.muni.cz>
2023-12-06 20:21:49 +0100

    feat: adding sending email

--- src/app/api/email/route.tsx
+++ src/app/api/email/route.tsx
@@ -1,16 +1,19 @@
 import { type NextApiResponse } from 'next';
 
 import { sendingEmailSchema } from '@/validators/sendingEmail';
+import {sendEmail} from "@/server/sendEmail";
 
 export const POST = async (request: Request, res: NextApiResponse) => {
 	const email = await request.json();
-	console.log(email);
 	const result = sendingEmailSchema.safeParse(email);
-	console.log(result);
 	if (!result.success) {
 		throw new Error('Invalid data from front-end');
 	} else {
-		//TODO send email
+		try {
+			await sendEmail(result.data)
+		} catch (error) {
+			return new Response(null, {status: 500})
+		}
 		return new Response(null, { status: 200 });
 	}
 };
