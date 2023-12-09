f17ad5d4225ddae8f754433aa7b31285debbf06d
Patrik Čangel <493182@mail.muni.cz>
2023-12-09 13:56:36 +0100

    fix: fix metadata

--- src/app/my_profile/page.tsx
+++ src/app/my_profile/page.tsx
@@ -1,53 +1,12 @@
-'use client';
-
-import { signIn, signOut, useSession } from 'next-auth/react';
 import { type Metadata } from 'next';
+import React from 'react';
 
-import Spinner from '@/components/Spinner';
-import { ChangeUserNameDialog } from '@/components/ChangeUserNameDialog';
+import { ProfilePage } from '@/components/ProfilePage';
 
 export const metadata: Metadata = {
 	title: 'My Profile'
 };
 
-const MyProfilePage = () => {
-	const { data, status } = useSession();
-	if (status === 'authenticated') {
-		return (
-			<div className="m-auto flex flex-col">
-				<div className="flex justify-between p-10">
-					<p className="mx-3 p-2">{data?.user.name}</p>
-					<p className="mx-3 p-2">{data?.user.email}</p>
-				</div>
-				<ChangeUserNameDialog />
-				<button
-					onClick={() => signOut()}
-					className="m-3 rounded bg-primaryBackground p-2 hover:bg-hoverPrimary"
-				>
-					Sign out
-				</button>
-			</div>
-		);
-	}
-
-	if (status === 'unauthenticated') {
-		return (
-			<div>
-				<button
-					onClick={() => signIn('discord')}
-					className="m-3 rounded p-2 hover:bg-hoverPrimary"
-				>
-					Sign in with Discord
-				</button>
-			</div>
-		);
-	}
-
-	return (
-		<div className="m-6 flex items-center justify-center">
-			<Spinner />
-		</div>
-	);
-};
+const MyProfilePage = () => <ProfilePage />;
 
 export default MyProfilePage;
